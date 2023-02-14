f3b1c5e21ab55c111a0ff3d595f591493599a3d5
agonzalezbesada <agonzalezbesada@danielcastelao.org>
2023-02-15 00:01:19 +0100

    Ejercicio 6 1.0

---
+++ src/Triangulo.java
@@ -0,0 +1,20 @@
+public class Triangulo extends Trigono implements Forma{
+
+    public void dibujar_puntos() {
+
+    }
+    public void rellenar_forma() {
+
+    }
+    public double calcular_area() {
+        double area = 0;
+        System.out.println(area);
+        return area;
+    }
+    public double calcular_perimetro() {
+        double perimetro = 0d;
+
+        return perimetro;
+    }
+
+}
