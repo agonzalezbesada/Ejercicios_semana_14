61315f5cd940f1ec5e68769e93de4e5dd2b0f9f2
agonzalezbesada <agonzalezbesada@danielcastelao.org>
2023-02-14 23:48:13 +0100

    Ejercicio 4 1.0

---
+++ src/Rectangulo.java
@@ -0,0 +1,22 @@
+public class Rectangulo extends Cuadrilatero implements Forma{
+
+    public void dibujar_puntos() {
+
+    }
+
+    public void rellenar_forma() {
+
+    }
+
+    public double calcular_area() {
+        double area = 0;
+
+        return area;
+    }
+
+    public double calcular_perimetro() {
+        double perimetro = 0;
+
+        return perimetro;
+    }
+}
